0056fe7b7600f54ea745214300130d0b5fa8e0d8
Zoyn <602723113@qq.com>
2018-03-18 11:34:19 +0800

    update: 删除CheckRunnable内关于tp重置重复的问题

--- src/me/may/afk/task/CheckRunnable.java
+++ src/me/may/afk/task/CheckRunnable.java
@@ -3,7 +3,6 @@ package me.may.afk.task;
 import me.may.afk.Entry;
 import me.may.afk.command.AFKCommand;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
@@ -20,6 +19,7 @@ public class CheckRunnable implements Runnable {
                 AFKCommand.afkPlayer.remove(name);
                 continue;
             }
+            /* 离线检查开始 */
             OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(name);
             if (offlinePlayer == null) {
                 AFKCommand.afkPlayer.remove(name);
@@ -31,9 +31,8 @@ public class CheckRunnable implements Runnable {
                 AFKCommand.map.get(name).delete();
                 continue;
             }
+            /* 离线检查结束 */
             Player player = offlinePlayer.getPlayer();
-            Location loc = AFKCommand.map.get(name).getLocation();
-            player.teleport(loc.clone().add(0D, -3.25D, 0D)); //TP回原位置
 
             List<String> commands = Entry.getInstance().getConfig().getStringList("Task.Commands");
             commands.forEach(s -> Bukkit.dispatchCommand(Bukkit.getConsoleSender(), s.replaceAll("&", "§").replaceAll("%player%", player.getName())));
